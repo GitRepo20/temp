58d54a87929e6c4bcef59a83b00a7eb75d397bc2
sjogade <sjogade@RILTP-7440.anant.saama.com>
2020-07-02 15:26:57 +0530

    Modified third testfile.txt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
   	  BrowserModule,    
       RouterModule.forRoot(
       routes,
-      { enableTracing: true } // <-- debugging purposes only test
+      { enableTracing: true } // <-- debugging purposes
     ),    
     HttpClientModule
   ],
